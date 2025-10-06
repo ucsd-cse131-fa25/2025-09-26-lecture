a28165ab2c5ef212c435a68def41292999f9d198
Joe Politz <joe.politz@gmail.com>
2025-10-06 11:07:00 -0700

    thxu copilot for instrs

--- Makefile
+++ Makefile
@@ -14,4 +14,4 @@ test/%.s: test/%.snek src/main.rs
 test/%.run: test/%.s runtime/start.rs
 	nasm -f $(ARCH) test/$*.s -o runtime/our_code.o
 	ar rcs runtime/libour_code.a runtime/our_code.o
-	rustc -L runtime/ runtime/start.rs -o test/$*.run
+	rustc --target x86_64-apple-darwin -L runtime/ runtime/start.rs -o test/$*.run
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,25 @@ use std::io::prelude::*;
 use sexp::*;
 use sexp::Atom::*;
 
+#[derive(Debug, Clone)]
+enum Reg {
+  Rax,
+  Rbx,
+  Rcx,
+  Rdx,
+  Rsi,
+  Rdi,
+  Rsp,
+  Rbp,
+}
+
+#[derive(Debug, Clone)]
+enum Instr {
+  Mov(Reg, i32),     // mov register, immediate
+  Add(Reg, i32),     // add register, immediate
+  Sub(Reg, i32),     // sub register, immediate
+}
+
 enum Expr {
   Num(i32),
   Add1(Box<Expr>),
@@ -26,13 +45,47 @@ fn parse_expr(s : &Sexp) -> Expr {
   }
 }
 
-fn compile_expr(e : &Expr) -> String {
+fn reg_to_string(reg: &Reg) -> &str {
+  match reg {
+    Reg::Rax => "rax",
+    Reg::Rbx => "rbx",
+    Reg::Rcx => "rcx",
+    Reg::Rdx => "rdx",
+    Reg::Rsi => "rsi",
+    Reg::Rdi => "rdi",
+    Reg::Rsp => "rsp",
+    Reg::Rbp => "rbp",
+  }
+}
+
+fn instr_to_string(instr: &Instr) -> String {
+  match instr {
+    Instr::Mov(reg, val) => format!("mov {}, {}", reg_to_string(reg), val),
+    Instr::Add(reg, val) => format!("add {}, {}", reg_to_string(reg), val),
+    Instr::Sub(reg, val) => format!("sub {}, {}", reg_to_string(reg), val),
+  }
+}
+
+fn instrs_to_string(instrs: &Vec<Instr>) -> String {
+  instrs.iter()
+    .map(instr_to_string)
+    .collect::<Vec<String>>()
+    .join("\n")
+}
+
+fn compile_expr(e : &Expr) -> Vec<Instr> {
   match e {
-	Expr::Num(n) => format!("mov rax, {}", *n),
-	Expr::Add1(subexpr) =>
-        compile_expr(subexpr) + "\nadd rax, 1",
-	Expr::Sub1(subexpr) =>
-        compile_expr(subexpr) + "\nsub rax, 1"
+	Expr::Num(n) => vec![Instr::Mov(Reg::Rax, *n)],
+	Expr::Add1(subexpr) => {
+      let mut instrs = compile_expr(subexpr);
+      instrs.push(Instr::Add(Reg::Rax, 1));
+      instrs
+    },
+	Expr::Sub1(subexpr) => {
+      let mut instrs = compile_expr(subexpr);
+      instrs.push(Instr::Sub(Reg::Rax, 1));
+      instrs
+    }
   }
 }
 
@@ -47,7 +100,8 @@ fn main() -> std::io::Result<()> {
   in_file.read_to_string(&mut in_contents)?;
 
   let expr = parse_expr(&parse(&in_contents).unwrap());
-  let result = compile_expr(&expr);
+  let instrs = compile_expr(&expr);
+  let result = instrs_to_string(&instrs);
   let asm_program = format!("
 section .text
 global our_code_starts_here
